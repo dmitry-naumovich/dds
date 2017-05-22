a0934ea61307942e82b76b6515bdb72f3d9ad87b
Dzmitry Naumovich <dmitry.nmv@gmail.com>
2017-05-22 17:35:47 +0300

    Added incrementHopCount() method to RouteReply for RREP proceeding

--- src/main/java/com/naumovich/domain/message/aodv/RouteReply.java
+++ src/main/java/com/naumovich/domain/message/aodv/RouteReply.java
@@ -76,6 +76,10 @@ public class RouteReply extends AodvMessage {
         this.lifetime = lifetime;
     }
 
+    public void incrementHopCount() {
+        hopCount++;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
