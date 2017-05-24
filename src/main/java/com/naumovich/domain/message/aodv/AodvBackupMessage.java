aa7a0671e1efe9cc20ea8fbba033e24e7ba36fda
Dzmitry Naumovich <dmitry.nmv@gmail.com>
2017-05-24 18:04:54 +0300

    newChunkId field added to backup message model

--- src/main/java/com/naumovich/domain/message/aodv/AodvBackupMessage.java
+++ src/main/java/com/naumovich/domain/message/aodv/AodvBackupMessage.java
@@ -7,6 +7,7 @@ public class AodvBackupMessage extends AodvMessage {
 
     private final static int TYPE = 5;
     private String chunkId;
+    private String newChunkId;
     private String sourceNode;
     private String destNode;
     private String newChunkSaver;
