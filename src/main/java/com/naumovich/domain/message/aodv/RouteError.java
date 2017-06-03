415dc080833c798732306724e64c450517f346e9
Dmitry Naumovich <dmitry.nmv@gmail.com>
2017-06-03 20:56:55 +0300

    RouteError bean refactored and completed

--- src/main/java/com/naumovich/domain/message/aodv/RouteError.java
+++ src/main/java/com/naumovich/domain/message/aodv/RouteError.java
@@ -1,19 +1,58 @@
 package com.naumovich.domain.message.aodv;
 
-import com.naumovich.domain.Node;
+import java.util.Objects;
 
-/**
- * Created by dzmitry on 2.5.17.
- */
 public class RouteError extends AodvMessage {
 
     public final static int TYPE = 3;
-    private boolean nFlag;
-    private Node destNode;
+    private String destNode;
     private int destSN;
+
+    public RouteError(String destNode, int destSN) {
+        this.destNode = destNode;
+        this.destSN = destSN;
+    }
+
+    public String getDestNode() {
+        return destNode;
+    }
+
+    public void setDestNode(String destNode) {
+        this.destNode = destNode;
+    }
+
+    public int getDestSN() {
+        return destSN;
+    }
+
+    public void setDestSN(int destSN) {
+        this.destSN = destSN;
+    }
 
     @Override
     public int getMessageType() {
         return TYPE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RouteError that = (RouteError) o;
+        return destSN == that.destSN &&
+                Objects.equals(destNode, that.destNode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(destNode, destSN);
+    }
+
+    @Override
+    public String toString() {
+        return "RouteError{" +
+                "destNode=" + destNode +
+                ", destSN=" + destSN +
+                '}';
     }
 }
